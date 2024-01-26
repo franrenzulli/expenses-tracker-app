309d7003d4e974360e560b37372b8e7b2cb46203
Franco Renzulli <85003859+franrenzulli@users.noreply.github.com>
2024-01-26 20:52:56 -0300

    Add files via upload

--- client/signup/signupApp.js
+++ client/signup/signupApp.js
@@ -19,7 +19,7 @@ btnSignup.addEventListener("click", async()=>{
             if(passwordChecker(password)){
 
                 // Send POST req
-                const response = await fetch("http://localhost:3000/signup", {
+                const response = await fetch(`http://localhost:3000/signup`, {
                     method:"POST",
                     headers:{
                         "Content-Type": "application/json",
