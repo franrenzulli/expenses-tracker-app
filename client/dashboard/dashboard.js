9b8893a9e48f19989ecbf8b680b7f71b3bf394c4
Franco Renzulli <85003859+franrenzulli@users.noreply.github.com>
2024-01-26 20:50:56 -0300

    Delete client/dashboard/dashboard.js

--- client/dashboard/dashboard.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        // Si no hay token, redirige al login u toma alguna otra acción
-        window.location.href = "/login";
-    }
-});
