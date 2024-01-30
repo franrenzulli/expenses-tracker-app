1ce191a657866dc9a5d3e4475cfa91fa6015c025
Franco Renzulli <85003859+franrenzulli@users.noreply.github.com>
2024-01-30 19:49:21 -0300

    Add files via upload

--- client/views/categories.js
+++ client/views/categories.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", async() => {
                 <span>Category Name</span>
                 <h3>${category.type}</h3>
                 <span>Type of transaction</span>
-                <h3 style="color:${category.color}">${category.color}</h4>
+                <h4 style="color:${category.color}">${category.color}</h4>
                 <span>Category color</span>
                 <div id="categoryButtons">
                 <button class="editBtn">Edit</button>
@@ -73,15 +73,43 @@ document.addEventListener("DOMContentLoaded", async() => {
             card.remove()
         })
     })
-/*
-    // This will delete a category from the database and its card in frontend
-    deleteBtn.addEventListener("click", function(){
-        const card = this.parentNode
-        const categoryh2 = card.querySelector("h2").textContent
-        fetchDeleteCategory(categoryh2)
-        card.remove()
+
+    const editBtn = document.querySelectorAll(".editBtn")
+    const editCategoryModal = document.getElementById("editCategoryModal")
+    const editCategory = document.getElementById("editCategory")
+    const closeEditModal = document.getElementById("closeEditModal")
+    const editCategoryName = document.getElementById("editCategoryName")
+    const editCategoriesSelect = document.getElementById("editCategoriesSelect")
+    const editColors = document.getElementById("editColors")
+
+    // This will edit a category from the database and its card in frontend
+    editBtn.forEach(editBtn =>{
+        editBtn.addEventListener("click", function(){
+            editCategoryModal.style.display = "flex"
+            categoriesList.style.display = "none"
+            const div = this.parentNode
+            const card = div.parentNode
+            const oldCategoryName = card.querySelector("h2").textContent
+            editCategoryName.value = card.querySelector("h2").textContent
+            editCategoriesSelect.value = card.querySelector("h3").textContent
+            editColors.value = card.querySelector("h4").textContent
+
+            editCategory.addEventListener("click", async()=>{
+                const newCategoryName = editCategoryName.value
+                const newType = editCategoriesSelect.value
+                const newColor = editColors.value
+
+                fetchEditCategory(username, oldCategoryName, newCategoryName, newType, newColor)
+                window.location.href = "/categories?username=" + username
+            })
+
+            closeEditModal.addEventListener("click", ()=>{
+                editCategoryModal.style.display = "none"
+                categoriesList.style.display = "flex"
+            })
+        })
     })
-*/
+
     // We put this function separate because the eventListener can't be asynchronous since it wouldn't have access to .this property
     const fetchDeleteCategory = async(categoryh2)=>{
         try{
@@ -92,6 +120,24 @@ document.addEventListener("DOMContentLoaded", async() => {
                 },
                 body: JSON.stringify({
                     categoryh2, username
+                }),
+            });
+        }catch(err){
+            console.error(err)
+        }
+    }
+
+    // We put this function separate because the eventListener can't be asynchronous since it wouldn't have access to .this property
+
+    const fetchEditCategory = async(username, oldCategoryName, newCategoryName, newType, newColor)=>{
+        try{
+            const response = fetch("http://localhost:3000/editCategory", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username, oldCategoryName, newCategoryName, newType, newColor
                 }),
             });
         }catch(err){
@@ -125,7 +171,8 @@ settings.addEventListener("click", async()=>{
 const categoriesList = document.getElementById("categoriesList")
 const addCategory = document.getElementById("addCategory")
 const addCategoryModal = document.getElementById("addCategoryModal")
-const closerModal = document.getElementById("closeModal")
+const closeAddModal = document.getElementById("closeAddModal")
+
 
 addCategory.addEventListener("click", ()=>{
     categoriesList.style.display = "none"
@@ -133,7 +180,7 @@ addCategory.addEventListener("click", ()=>{
     addCategoryModal.style.display = "flex"
 })
 
-closeModal.addEventListener("click", ()=>{
+closeAddModal.addEventListener("click", ()=>{
     categoriesList.style.display = "flex"
     addCategoryModal.style.display = "none"
 })
@@ -171,13 +218,4 @@ saveCategory.addEventListener("click", async()=>{
     addCategoryModal.style.display = "none"
     window.location.href = "/categories?username=" + username
 })
-
-/*
-const editCategory = document.getElementById("editCategory")
-const editCategoryModal = document.getElementById("editCategoryModal")
-
-editCategory.addEventListener("click", ()=>{
-    categoriesList.style.display = "none"
-    editCategoryModal.style.display = "flex"
-})*/
 
