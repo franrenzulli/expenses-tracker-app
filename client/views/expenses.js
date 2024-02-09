2ca8ef41e26a3c95ced6ec9fc33ab3feb4d27321
Franco Renzulli <85003859+franrenzulli@users.noreply.github.com>
2024-02-09 02:03:00 -0300

    Add files via upload

--- client/views/expenses.js
+++ client/views/expenses.js
@@ -1,3 +1,5 @@
+
+
 // This function will check if you have an existing token to enter the website
 document.addEventListener("DOMContentLoaded", async() => {
     const token = localStorage.getItem("token");
@@ -22,6 +24,7 @@ document.addEventListener("DOMContentLoaded", async() => {
         const emptyList = document.getElementById("emptyList")
         const transactionList = document.getElementById("transactionList")
         const categoriesSelect = document.getElementById("categoriesSelect")
+        const editCategoriesSelect = document.getElementById("editCategoriesSelect")
         const categories = await responseCategories.json()
 
            // If there isn't any category in the database, display this
@@ -37,6 +40,7 @@ document.addEventListener("DOMContentLoaded", async() => {
         categories.forEach(category => {
             const option = `<option>${category.name}</option>`
             categoriesSelect.insertAdjacentHTML('beforeend', option);
+            editCategoriesSelect.insertAdjacentHTML('beforeend', option);
         })
 
         // Load the transactions in the transactionList
@@ -59,12 +63,14 @@ document.addEventListener("DOMContentLoaded", async() => {
             <div class="transactionCard">
             <div class="infoSection">
                 <div class="transNam">
-                <h2>${transaction.name}</h2>
+                    <h2>${transaction.name}</h2>
                 </div>
                 <div class="transCat">
-                <h2>${transaction.category}</h2>
+                    <h2>${transaction.category}</h2>
                 </div>
-                <h2>$${transaction.amount}</h2>
+                <div class="transAm">
+                    <h2>$${transaction.amount}</h2>
+                </div>
             </div>
             <div class="buttons">
                 <button class="editBtn">Edit</button>
@@ -74,6 +80,83 @@ document.addEventListener("DOMContentLoaded", async() => {
             `
             list.insertAdjacentHTML("beforeend", transactionCard)
         })
+
+        const deleteBtns = document.querySelectorAll(".deleteBtn")
+        deleteBtns.forEach(deleteBtn =>{
+            // This will delete a category from the database and its card in frontend
+            deleteBtn.addEventListener("click", function(){
+                const div = this.parentNode
+                const card = div.parentNode
+                const transactionh2 = card.querySelector(".transNam h2").textContent
+                const categoryName = card.querySelector(".transCat h2").textContent
+
+                const transactionAmountString = card.querySelector(".transAm h2").textContent
+                const transactionAmount = parseFloat(transactionAmountString.replace(/\$/g, ''));
+
+                fetchDeleteTransaction(transactionh2, categoryName, transactionAmount)
+                card.remove()
+            })
+        })
+
+        const fetchDeleteTransaction = async(transactionh2, categoryName, transactionAmount)=>{
+            try{
+                const response = fetch("http://localhost:3000/deleteTransaction", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        transactionh2, username, categoryName, transactionAmount
+                    }),
+                });
+            }catch(err){
+                console.error(err)
+            }
+        }
+
+        const editTransactionModal = document.getElementById("editTransactionModal")
+        const editTransactionName = document.getElementById("editTransactionNamee")
+        const editTransactionAmount = document.getElementById("editTransactionAmount")
+
+        const editBtns = document.querySelectorAll(".editBtn")
+        editBtns.forEach(editBtn =>{
+            // This will edit a transaction from the database and its card in frontend
+            editBtn.addEventListener("click", function(){
+
+                const div = this.parentNode
+                const card = div.parentNode
+                const transactionh2 = card.querySelector(".transNam h2").textContent
+                document.getElementById("oldTransactionName").value = transactionh2
+
+
+                editTransactionName.value = card.querySelector(".transNam h2").textContent
+                editCategoriesSelect.value = card.querySelector(".transCat h2").value
+                editTransactionAmount.value = card.querySelector(".transAm h2").textContent
+
+                editTransactionModal.style.display = "flex"
+                emptyList.style.display = "none"
+                transactionList.style.display = "none"
+
+            })
+        })
+
+     /*   const fetchDeleteTransaction = async(transactionh2)=>{
+            try{
+                const response = fetch("http://localhost:3000/deleteTransaction", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        transactionh2, username
+                    }),
+                });
+            }catch(err){
+                console.error(err)
+            }
+        } */
+
+
 });
 
 const dashboard = document.getElementById("dashboard")
@@ -100,6 +183,7 @@ settings.addEventListener("click", async()=>{
 const addTransaction = document.getElementById("addTransaction")
 const addTransactionModal = document.getElementById("addTransactionModal")
 const closeAddModal = document.getElementById("closeAddModal")
+const closeEditModal = document.getElementById("closeEditModal")
 const saveTransaction = document.getElementById("saveTransaction")
 
 addTransaction.addEventListener("click", ()=>{
@@ -111,6 +195,13 @@ addTransaction.addEventListener("click", ()=>{
 closeAddModal.addEventListener("click", ()=>{
     emptyList.style.display = "none"
     addTransactionModal.style.display = "none"
+    transactionList.style.display = "flex"
+})
+
+closeEditModal.addEventListener("click", ()=>{
+    emptyList.style.display = "none"
+    addTransactionModal.style.display = "none"
+    editTransactionModal.style.display = "none"
     transactionList.style.display = "flex"
 })
 
@@ -134,3 +225,26 @@ saveTransaction.addEventListener("click", async()=>{
     window.location.href = "/expenses?username=" + username
 })
 
+const editTransaction = document.getElementById("editTransaction")
+
+editTransaction.addEventListener("click", async()=>{
+
+    const newTransactionName = document.getElementById("editTransactionNamee").value
+    const categoryName = document.getElementById("editCategoriesSelect").value
+    const transactionAmountString = document.getElementById("editTransactionAmount").value;
+    const transactionAmount = parseFloat(transactionAmountString.replace(/\$/g, ''));
+    const oldTransactionName = document.getElementById("oldTransactionName").value
+
+     // Send POST request to /addTransaction
+     const response = await fetch("http://localhost:3000/editTransaction", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username, oldTransactionName, newTransactionName, categoryName, transactionAmount
+        }),
+    });
+
+    window.location.href = "/expenses?username=" + username
+})
